0c3bdf71010c1fecb7fd61e62109cc9ae246e69a
Amayuki-beginner <surferjim810@gmail.com>
2025-06-24 11:17:59 +0100

    [Date: 24/06/2025]

---
+++ Practices/Others/LeetCode/Easy/PalidromeNumber.java
@@ -0,0 +1,21 @@
+package Practices.Others.LeetCode.Easy;
+
+public class PalidromeNumber {
+    class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+}
